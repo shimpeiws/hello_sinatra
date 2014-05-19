f55eddea393ee3ff53a3af36aa5044f6d03a6066
shimpeiws <shimpeiws@gmail.com>
2014-05-20 08:51:44 +0900

    add routings

--- web.rb
+++ web.rb
@@ -6,6 +6,35 @@ require 'open-uri'
 require 'json'
 
 get '/' do
-  erb :index
+  #  インスタンス変数で渡す
+  @bar = "bar"
+  # ローカル変数として渡す
+  erb :index, :locals => {:foo  => 'foo'} 
 end
 
+#  複数ルートで同一動作
+['/one', '/two', '/three'].each do |route|
+  get route do
+    "Triggered #{route} via GET"
+  end
+
+  post route do
+    "Triggered #{route} via POST"
+  end
+end
+
+post '/login' do
+  # POSTのパラメータはparamsでとれる
+  username = params[:username]
+  password = params[:password]
+end
+
+get '/name/:name' do
+  # /name/some_name?foo=XYZ形式をとる
+  "You asked for #{params[:name]} as well as #{params[:foo]}"
+end
+
+# 正規表現ありのroute
+get %r{/(sp|gr)eedy} do
+  "You got caught in the greedy route!"
+end
